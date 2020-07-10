4f81f470247b40728437ad79fb4295074712ca91
Debashish Palit <dpalit07@gmail.com>
2020-07-10 20:09:43 +0530

    db connection lost error

--- config.py
+++ config.py
@@ -9,4 +9,6 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_POOL_RECYCLE = 299
+    SQLALCHEMY_POOL_TIMEOUT = 20
     CLIENT_ID = os.environ.get('CLIENT_ID')
